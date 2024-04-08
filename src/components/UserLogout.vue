a5afe5a73acd02fc250f25c79d1438c94e8a531a
Sunny Lam <77874599+sunnylam9876@users.noreply.github.com>
2024-04-07 21:33:36 -0700

    Added logout function

---
+++ src/components/UserLogout.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "userLogin",
+    methods: {
+        logout() {
+            let userName = localStorage.getItem("userName");
+            localStorage.removeItem("userId");
+            localStorage.removeItem("userName");
+            localStorage.removeItem("role");
+
+            window.alert("User: " + userName + " logged out successfully.")
+
+            // Redirect to the "/login" route                    
+            this.$router.push("/login");
+        }
+    },
+
+    mounted() {   // called when component is loaded
+        this.logout();
+    }
+}
+
+</script>
